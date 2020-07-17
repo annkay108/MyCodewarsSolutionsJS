b11c414d98e0a21b638736f5eba9d00843123684
annkay108 <annkhachhibhoya@gmail.com>
2020-07-17 08:51:09 +0545

    added simpler solution to prime in numbers

--- 5kyu/Primes_In_Numbers/code.js
+++ 5kyu/Primes_In_Numbers/code.js
@@ -36,3 +36,14 @@ function checkIfPrime(n){
     }
     return ans
   }
+
+  /* Here's more elegant and simpler solution
+  function primeFactors(n){
+    for (var i=2, res="", f; i <= n; i++) {
+      f=0;
+      while (n%i == 0) { f++; n/=i }
+      res += f ? "(" + ( f>1 ? i+"**"+f  : i ) +")" : ""
+    }
+    return res || "("+n+")"
+  }
+  */
